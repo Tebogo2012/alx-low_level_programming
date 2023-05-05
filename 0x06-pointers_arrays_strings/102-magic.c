52b70f97c70bc351ea524c869e812ee2d0b557ac
Tebogo2012 <tebogogti@gmail.com>
2023-05-04 19:28:31 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,23 @@
-#include "main.h"
-/**
- * rot13 - encodes a string using rot13
- * @s: input string.
- * Return: the pointer to dest.
- */
+#include <stdio.h>
 
-char *rot13(char *s)
+int main(void)
 {
-	int i, j;
+  int n;
+  int a[5];
+  int *p;
 
-	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; x[j] != '\0'; j++)
-		{
-			if (s[i] == x[j])
-			{
-				s[i] = y[j];
-				break;
-			}
-		}
-	}
-
-	return (s);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  p[5] = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
